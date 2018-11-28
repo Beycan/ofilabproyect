0fd49f7783cdbc436fe7fea1eb2c550a752458cf
Smithfk <smith.feick@gmail.com>
2018-11-28 17:34:10 -0500

    Pagina de Nosotros terminada

--- nosotros.php
+++ nosotros.php
@@ -1,9 +1,14 @@
 <?php include 'template/header.php' ?>
 
 <section>
-	<div class="container">
-		<h1>¿Quienes somos?</h1>
-		<div>
+	
+	<div class="container" >
+		
+		<div id="nosotros-font">
+				<div id="nfont"></div>
+		
+		<h1 id="tSomos">¿Quienes somos?</h1>
+		<div id="textSomos">
 			Empresa peruana fundada por un grupo de profesionales con amplia experiencia en equipamiento de laboratorio e industria. 
 			Contamos con profesionales altamente capacitados que brindan asesoría antes, durante y después de cada venta o servicio, esto asegura al cliente una total satisfacción. 
 			Representamos y comercializamos marcas de prestigio a nivel mundial. 
@@ -11,26 +16,42 @@
 			Fabricamos bajo estrictos procedimientos técnicos sustentados por las normativas nacionales e internacionales. 
 			Brindamos servicio de: Mantenimiento, reparación, calibración en equipos de diversas marcas.
 		</div>
-		<div class="mision-vision">
-			<div class="grid-container col-2">
-				<div>
-					<h2>Misión</h2>
+		<div id="cont">
+			<div class="mision-vision" >
+				<div class="grid-container col-2">
+					<div class="margin">
 					
-	Dar un excelente servicio de acuerdo con las exigencias y requerimientos de nuestros clientes, cumpliendo con los lineamientos de normas nacionales e internacionales vigentes y de esta manera proporcionar una atención de calidad basada en la mejora continua de nuestra empresa.
+					<div id="mision-font">
+						<div class="text" >
+							<h2  class="titulo">Misión</h2>
+						Dar un excelente servicio de acuerdo con las exigencias y requerimientos de nuestros clientes, cumpliendo con los lineamientos de normas nacionales e internacionales vigentes y de esta manera proporcionar una atención de calidad basada en la mejora continua de nuestra empresa.
+						</div>
+					</div>
+					</div>
 
-				</div>
-				<div>
-					<h2>Visión</h2>
-						Convertirnos en la empresa líder a nivel nacional en la calibración y fabricación de equipos de laboratorio y seguir innovando en el área de investigación con el objetivo de mejorar el avance tecnológico del país con productos propios con calidad de exportación. 
-						Ampliar nuevas áreas y desarrollar nuevos métodos de medición aplicando con nuevas tecnologías con el objetivo de cubrir las necesidades del cliente.
-
+					<div class="margin">
+					
+					<div id="vision-font">
+						<div class="text" >
+							<h2 class="titulo">Visión</h2>
+							Convertirnos en la empresa líder a nivel nacional en la calibración y fabricación de equipos de laboratorio y seguir innovando en el área de investigación con el objetivo de mejorar el avance tecnológico del país con productos propios con calidad de exportación. 
+							Ampliar nuevas áreas y desarrollar nuevos métodos de medición aplicando con nuevas tecnologías con el objetivo de cubrir las necesidades del cliente.
+						</div>
+					</div>
+					</div>
 				</div>
 			</div>
 		</div>
-		
 	</div>
+</div>
 	<div class="container">
-		<h2>Valores</h2>
+		
+		<h2 id="tValores">Valores</h2>
+		<div id="valores-tar">
+		<div id="imgValores">
+			<img src="./images/valores.jpg" width="100%">
+		</div>
+		<div id="txtValores">
 			•	Transparencia. hacia nuestro equipo y hacia nuestros clientes. <br>
 			•	Puntualidad. (El tiempo es dinero), Es importante para con nuestro equipo, sino también en otras situaciones que afectan directamente al cliente. <br>
 			•	Diferenciación: Somos personas con talento dispuestas a trabajar gustosamente por nuestros clientes. <br>
@@ -39,6 +60,8 @@
 			•	Pasión. Por nuestro trabajo y con lo que hacemos <br>
 			•	Lealtad. Hacia nuestros clientes y compañeros
 
+		</div>
+		</div>
 	</div>
 
 </section>
